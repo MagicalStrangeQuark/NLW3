4220a5c432731b9d696c60e7f5c65fae01030b8b
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-13 19:49:16 -0300

    migrations

---
+++ server/src/database/migrations/1602629070965-create_orphanages_table.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanagesTable1602629070965 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
