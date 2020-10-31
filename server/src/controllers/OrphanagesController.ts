98a35f71e7354f5bc08f6affb7013ab8d40ab076
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-31 19:36:48 -0300

    OrphanagesController.ts

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -5,6 +5,8 @@ import Orphanage from './../models/Orphanage';
 
 import orphanageView from './../views/orphanages';
 
+import * as Yup from 'yup';
+
 export default {
     async index(request: Request, response: Response) {
         const orphanages = await getRepository(Orphanage).find({
@@ -37,7 +39,7 @@ export default {
 
         const images = requestImages.map(image => { return { path: image.filename } });
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -46,7 +48,28 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         });
+
+        await schema.validate(data, {
+            abortEarly: false
+        })
+
+        const orphanage = orphanagesRepository.create(data);
 
         await orphanagesRepository.save(orphanage);
 
